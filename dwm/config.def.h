52ba9b7dfcb350be2b34c0d59a93bdc7d13a4d2b
Paulo Roberto <dante.hill.o@proton.me>
2025-10-30 15:20:51 -0300

    change font and add keybind for open browser

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst 	= 1;   /* 1: if pinning fails, display
 static const int showsystray        			= 1;        /* 0 means no systray */
 static const int showbar            			= 1;        /* 0 means no bar */
 static const int topbar             			= 1;        /* 0 means bottom bar */
-static const char *fonts[]          			= { "Hack Nerd Font Mono:size=10" };
-static const char dmenufont[]       			= "monospace:size=10";
+static const char *fonts[]          			= { "JetBrainsMono Nerd Font:antialias=true:autohint=true:size=10" };
+static const char dmenufont[]       			= "JetBrainsMono Nerd Font:antialias=true:autohint=true:size=10";
 static const char col_gray1[]       			= "#222222";
 static const char col_gray2[]       			= "#444444";
 static const char col_gray3[]       			= "#bbbbbb";
@@ -68,6 +68,8 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define BROWSER "firefox"
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
@@ -113,6 +115,10 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      		6)
 	TAGKEYS(                        XK_8,                      		7)
 	TAGKEYS(                        XK_9,                      		8)
+
+/* Application bindings */
+	{ MODKEY,			XK_w,          spawn,      {.v = (const char*[]){ BROWSER, NULL } } },
+
 };
 
 /* button definitions */
