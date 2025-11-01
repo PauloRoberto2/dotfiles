afdc5da281dc7d41220bd06769d94ef0e7cec8a7
Paulo Roberto <dante.hill.o@proton.me>
2025-11-01 09:21:21 -0300

    anysize path add and change font

--- st/config.def.h
+++ st/config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
-/*static char *font = "JetBrainsMono Nerd:pixelsize=16:antialias=true:autohint=true";*/
+/*static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";*/
+static char *font = "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
